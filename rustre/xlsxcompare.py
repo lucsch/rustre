f37f9ca93b1de64e28891e0fbbc66f445fbfff07
Lucien SCHREIBER <lucien.schreiber@terranum.ch>
2022-05-19 14:24:27 +0200

    Adding preliminary code for comparing xlsx files

--- rustre/xlsxcompare.py
+++ rustre/xlsxcompare.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import logging
+import configparser
 from rustre.xlsxfile import XlsxFile
 
 ####################################################################################################
 # @brief Compare two xlsx files
-# @details the comparison is made on a column basis
 ####################################################################################################
 
 
@@ -13,10 +13,19 @@ class XlsxCompare:
 
     ####################################################################################################
     # @brief Init the XlsxCompare object
-    #    @param[in] file1  First file (xlsx only)
-    #    @param[in] file2  Second file (xlsx only)
+    #    @param[in] config_file  config file (ini)
+    #    @param[in] file_source  source file (xlsx)
+    #    @param[in] file_target  target file (xlsx)
     ####################################################################################################
-    def __init__(self, file1, file2, sheet_index1=0, sheet_index2=0):
+    def __init__(self, config_file, file_source, file_target):
+        self.m_config_file = config_file
+        self.m_file_source = file_source
+        self.m_file_target = file_target
+
+    def do_compare(self, log_file):
+        # open the config file
         pass
+
+
 
 
--- test/test_xlsxcompare.py
+++ test/test_xlsxcompare.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import shutil
 
 import pytest
 
@@ -8,6 +9,31 @@ from .context import rustre
 from .context import UNIT_TEST_PATH_OUTPUT
 from .context import UNIT_TEST_PATH
 
+
+@pytest.fixture(scope="module")
+def get_src_filename():
+    src_filename = os.path.join(UNIT_TEST_PATH_OUTPUT, "test_compare_src.xlsx")
+    shutil.copyfile(os.path.join(UNIT_TEST_PATH, "test_compare_src.xlsx"), src_filename)
+    return src_filename
+
+
+@pytest.fixture(scope="module")
+def get_target_filename():
+    return os.path.join(UNIT_TEST_PATH, "test_compare_target.xlsx")
+
+
+@pytest.fixture(scope="module")
+def get_config_file():
+    return os.path.join(UNIT_TEST_PATH, "test_compare.ini")
+
+
+def test_file_exists(get_src_filename):
+    assert os.path.exists(get_src_filename)
+
+
+def test_compare(get_src_filename, get_config_file, get_target_filename):
+    xcomp = rustre.xlsxcompare.XlsxCompare(get_config_file, get_src_filename, get_target_filename)
+    assert xcomp.do_compare(os.path.join(UNIT_TEST_PATH_OUTPUT, "compare_log1.xlsx"))
 
 
 
