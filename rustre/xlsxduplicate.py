0ce0e8c92c1d87c17e0b2be2e8add4f5de761e00
Lucien SCHREIBER <lucien.schreiber@terranum.ch>
2022-08-20 10:13:21 +0200

    see #3 checking duplicate is now working

--- rustre/xlsxduplicate.py
+++ rustre/xlsxduplicate.py
@@ -29,14 +29,41 @@ class XlsxDuplicate:
         if not self._is_valid():
             return False
 
-        # open log file
-        XlsxFile.create_file(self.m_log_filename)
-        xlsx_log = XlsxFile(self.m_log_filename)
-        xlsx_log.append_row(["ID", "COUNT"])
-
         # open source file
         xlsx_src = XlsxFile(self.m_src_filename, self.m_sheet_index, load_in_memory=True)
 
+        # open log file
+        XlsxFile.create_file(self.m_log_filename)
+        xlsx_log = XlsxFile(self.m_log_filename)
+        header = self._get_col_id(xlsx_src.get_columns(self.m_header_index))
+        header.append("COUNT")
+        xlsx_log.append_row(header)
+
+        # iterate
+        skip_index = []
+        for row_index in range(self.m_header_index+1, xlsx_src.get_row_count() + 1):
+            if row_index in skip_index:
+                continue
+            row = xlsx_src.get_columns(row_index)
+            id_val = self._get_col_id(row)
+            count = 0
+            for row_check_index in range(row_index, xlsx_src.get_row_count() + 1):
+                row_check = xlsx_src.get_columns(row_check_index)
+                id_check = self._get_col_id(row_check)
+                if id_check == id_val:
+                    count += 1
+                    skip_index.append(row_check_index)
+
+            # add to log if more than one entry
+            if count > 1:
+                id_val.append(count)
+                xlsx_log.append_row(id_val)
+
         xlsx_log.save()
         return True
+
+    def _get_col_id(self, row :[]) -> []:
+        return [row[i] for i in self.m_cols]
+
+
 
--- test/test_xlsxduplicate.py
+++ test/test_xlsxduplicate.py
@@ -15,6 +15,6 @@ def get_test_file():
 
 
 def test_duplicate(get_test_file):
-    xdupli = rustre.xlsxduplicate.XlsxDuplicate(get_test_file, os.path.join(UNIT_TEST_PATH_OUTPUT, "log_duplicate.xlsx"), [1,2,4])
+    xdupli = rustre.xlsxduplicate.XlsxDuplicate(get_test_file, os.path.join(UNIT_TEST_PATH_OUTPUT, "log_duplicate.xlsx"), [0,1,3])
     assert xdupli.check_duplicate()
 
